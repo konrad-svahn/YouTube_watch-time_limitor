660724d863204103bfe823ec0ccf39a96c9f7b81
konrad <svahnkon@arcada.fi>
2023-08-30 22:56:32 +0300

    commit 3

--- main.js
+++ main.js
@@ -3,4 +3,25 @@ const sites = {
   github: 'https://github.com/konrad-svahn/',  
 };
 
-location.replace(sites.kielikoulu);
+setTimeout(checkTime, 1000);
+
+function checkTime() {
+  let stamp = localStorage.getItem("timeStamp");
+  let now = Date.now();
+  let diff = now - stamp;
+
+  if (!document.hidden) {
+    if (stamp) {
+      if (diff > 60000) {
+        localStorage.setItem("timeStamp", now);
+        location.replace(sites.kielikoulu);
+      }
+    } else {
+      localStorage.setItem("timeStamp", now);
+    }
+  } else {
+    
+  }
+  console.log(diff)
+  setTimeout(checkTime, 1000);
+}
