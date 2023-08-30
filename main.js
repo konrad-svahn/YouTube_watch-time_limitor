868d1353f067678663ab2ec109f68bcf01b7a7c3
konrad <svahnkon@arcada.fi>
2023-08-31 01:11:06 +0300

    commit 4

--- main.js
+++ main.js
@@ -3,25 +3,41 @@ const sites = {
   github: 'https://github.com/konrad-svahn/',  
 };
 
+let mod = 0;
 setTimeout(checkTime, 1000);
 
 function checkTime() {
   let stamp = localStorage.getItem("timeStamp");
+  let pause = localStorage.getItem("pauseStamp");
   let now = Date.now();
   let diff = now - stamp;
 
   if (!document.hidden) {
+
+    if (pause) {
+      console.log("tabed in")
+      localStorage.removeItem("pauseStamp");
+      mod = (now - pause) + mod
+    }
+
     if (stamp) {
-      if (diff > 60000) {
+      if (diff - mod > 60000 && !pause) {
+        mod = 0
         localStorage.setItem("timeStamp", now);
-        location.replace(sites.kielikoulu);
+        console.log("reset");
+        //location.replace(sites.kielikoulu); //<----------------------------- alter site
       }
     } else {
       localStorage.setItem("timeStamp", now);
     }
+
   } else {
-    
+    if (!pause) {
+      console.log("tabed out")
+      localStorage.setItem("pauseStamp", now);
+    }
   }
-  console.log(diff)
+  console.log("mod is: " + mod)
+  console.log(diff - mod)
   setTimeout(checkTime, 1000);
 }
