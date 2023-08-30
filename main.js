832802f8ace1716c62af32643b1ef7f29fcd11d1
konrad <svahnkon@arcada.fi>
2023-08-31 01:32:19 +0300

    commit 5

--- main.js
+++ main.js
@@ -3,41 +3,49 @@ const sites = {
   github: 'https://github.com/konrad-svahn/',  
 };
 
+// the amount of milliseconds before you get redireckted
+const timeLimit = 10800000
+
+// mod is a modifier that causes the timer to not advance when not in the youtube tab
 let mod = 0;
 setTimeout(checkTime, 1000);
 
 function checkTime() {
+  // marker of when the timer started
   let stamp = localStorage.getItem("timeStamp");
+
+  // marker of when the latest pause to the timer started
   let pause = localStorage.getItem("pauseStamp");
+  
   let now = Date.now();
   let diff = now - stamp;
 
+  // this happens if you are in the tab ---> true if the tab is active
   if (!document.hidden) {
 
+    // this runs if you tab in to the youtube tab after having tabed out
     if (pause) {
-      console.log("tabed in")
       localStorage.removeItem("pauseStamp");
       mod = (now - pause) + mod
     }
 
     if (stamp) {
-      if (diff - mod > 60000 && !pause) {
+      // runs if the time spent in the tab exedes the time limit
+      if (diff - mod > timeLimit && !pause) {
         mod = 0
         localStorage.setItem("timeStamp", now);
-        console.log("reset");
-        //location.replace(sites.kielikoulu); //<----------------------------- alter site
+        location.replace(sites.kielikoulu); //<---------------------------------------------------------------------------------------------------------------- alter site
       }
     } else {
       localStorage.setItem("timeStamp", now);
     }
 
+  // this runs if you are not in the tab
   } else {
+    // on the first frame you are tabed out a timestamp for that time is generated 
     if (!pause) {
-      console.log("tabed out")
       localStorage.setItem("pauseStamp", now);
     }
   }
-  console.log("mod is: " + mod)
-  console.log(diff - mod)
   setTimeout(checkTime, 1000);
 }
