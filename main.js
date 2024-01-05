94be9eddba5d5447c5dc6b9bb2e6981351480cbe
konrad <svahnkon@arcada.fi>
2024-01-05 05:23:28 +0200

    half the settings work

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const sites = {
-  kielikoulu: 'https://kielikoulu.yle.fi/#/history',
-};
-
-// the amount of milliseconds before you get redireckted
-const timeLimit = 10800000
-
 // mod is a modifier that causes the timer to not advance when not in the youtube tab
 let mod = 0;
-setTimeout(checkTime, 1000);
 
-function checkTime() {
+function getVar() {
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+  browser.storage.sync.get(["target","sessionHours","sessionMinutes"]).then(checkTime, onError);
+}
+
+function checkTime(setings) {
+  // the site you get trown to when the time is upp
+  let target = setings.target;
+
+  // the amount of milliseconds before you get redireckted
+  let timeLimit = (parseInt(setings.sessionHours) * 60000 * 60) + (parseInt(setings.sessionMinutes) * 60000);
+
   // marker of when the timer started
   let stamp = localStorage.getItem("timeStamp");
 
@@ -25,15 +30,15 @@ function checkTime() {
     // this runs if you tab in to the youtube tab after having tabed out
     if (pause) {
       localStorage.removeItem("pauseStamp");
-      mod = (now - pause) + mod
+      mod = (now - pause) + mod;
     }
 
     if (stamp) {
       // runs if the time spent in the tab exedes the time limit
       if (diff - mod > timeLimit && !pause) {
-        mod = 0
+        mod = 0;
         localStorage.removeItem("timeStamp");
-        location.replace(sites.kielikoulu); //<---------------------------------------------------------------------------------------------------------------- alter site
+        location.replace(target); //<---------------------------------------------------------------------------------------------------------------- alter site
       }
     } else {
       localStorage.setItem("timeStamp", now);
@@ -47,8 +52,11 @@ function checkTime() {
     }
   }
   // the number shown in the console it shows the nuber of minutes since the last checkpoint
-  let display = (diff - mod)/60000
-  let displayLimit = timeLimit/60000
-  console.log(display.toFixed(1) + " / " + displayLimit)
-  setTimeout(checkTime, 60000);
+  let display = (diff - mod)/60000;
+  let displayLimit = timeLimit/60000;
+  console.log(display.toFixed(1) + " / " + displayLimit);
+  setTimeout(getVar, 10000);
 }
+
+// start the counter
+getVar();
--- options.js
+++ options.js
@@ -2,21 +2,25 @@ function saveOptions(e) {
     e.preventDefault();
     browser.storage.sync.set({
         target: document.querySelector("#target").value,
-        hours: document.querySelector("#hours").value,
-        minutes: document.querySelector("#minutes").value
+        sessionHours: document.querySelector("#sessionHours").value,
+        sessionMinutes: document.querySelector("#sessionMinutes").value,
+        breakHours: document.querySelector("#breakHours").value,
+        breakMinutes: document.querySelector("#breakMinutes").value
     });
 }
   
 function restoreOptions() {
     function setCurrentChoice(result) {
         document.querySelector("#target").value = result.target || "about:home"; // https://kielikoulu.yle.fi/#/history
-        document.querySelector("#hours").value = result.hours || 3;
-        document.querySelector("#minutes").value = result.minutes || 0;
+        document.querySelector("#sessionHours").value = result.sessionHours || 3;
+        document.querySelector("#sessionMinutes").value = result.sessionMinutes || 0;
+        document.querySelector("#breakHours").value = result.breakHours || 1;
+        document.querySelector("#breakMinutes").value = result.breakMinutes || 30;
     }
     function onError(error) {
         console.log(`Error: ${error}`);
     }
-    browser.storage.sync.get(["target","hours","minutes"]).then(setCurrentChoice, onError);
+    browser.storage.sync.get(["target","sessionHours","sessionMinutes","breakHours","breakMinutes"]).then(setCurrentChoice, onError);
 }
   
 document.addEventListener("DOMContentLoaded", restoreOptions);
